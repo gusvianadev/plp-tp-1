44bf7fd6e7426a803a4a750655fa0b58becd229b
Gustavo Viana <gusvianadev@gmail.com>
2025-09-04 14:28:39 -0300

    corregido

--- src/Util.hs
+++ src/Util.hs
@@ -3,14 +3,14 @@ module Util where
 -- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
 -- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
 alinearDerecha :: Int -> String -> String
-alinearDerecha n s = replicate (length s - n) ' ' ++ s
+alinearDerecha n s = replicate (n - length s) ' ' ++ s
 
 -- | Dado un índice y una función, actualiza el elemento en la posición del índice
 -- aplicando la función al valor actual. Si el índice está fuera de los límites
 -- de la lista, devuelve la lista sin cambios.
 -- El primer elemento de la lista es el índice 0.
 actualizarElem :: Int -> (a -> a) -> [a] -> [a]
-actualizarElem n f xs = error "COMPLETAR EJERCICIO 2 AAAA"
+actualizarElem n f xs = error "COMPLETAR EJERCICIO 2"
 
 -- | infinito positivo (Haskell no tiene literal para +infinito)
 infinitoPositivo :: Float
